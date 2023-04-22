5655f487878d1661ccb6d334af7f081b88c9d8b0
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-04-22 19:25:09 +0300

    add model data

---
+++ ContactList/Person.swift
@@ -0,0 +1,50 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by MikhaiL on 22.04.2023.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phone: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataStore.shared.names.shuffled()
+        let surnames = DataStore.shared.surnames.shuffled()
+        let emails = DataStore.shared.emails.shuffled()
+        let phones = DataStore.shared.phones.shuffled()
+        
+        let iterationCount = min(
+            names.count,
+            surnames.count,
+            emails.count,
+            phones.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phone: phones[index]
+            )
+            persons.append(person)
+        }
+        return persons
+    }
+}
+
+enum Contacts: String {
+    case phone = "phone"
+    case email = "tray"
+}
